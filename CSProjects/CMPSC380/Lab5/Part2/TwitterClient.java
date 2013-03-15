a1da9bbe8f809d2a8364ca264b8900f36728631f
Braden Licastro <blicastro@live.com>
2013-03-15 12:17:33 -0400

    Need help, HELP ME!

--- CSProjects/CMPSC380/Lab5/Part2/TwitterClient.java
+++ CSProjects/CMPSC380/Lab5/Part2/TwitterClient.java
@@ -15,6 +15,12 @@
 import java.util.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import twitter4j.*;
 
@@ -27,90 +33,156 @@ public class TwitterClient {
      * Main method, initiates the main menu and calls classes from there
      */
     public static void main(String[] args) {
-    	TwitterClient tc = new TwitterClient();
-    	
-		// Initialize a menu and run cycle marker
-		int menu;
-		boolean quit = false;
+    	try{
+			TwitterClient tc = new TwitterClient();
+			
+			// Load the HSQL Database Engine JDBC driver
+            Class.forName("org.hsqldb.jdbc.JDBCDriver");
+
+            // Connect to the database
+            // It will be create automatically if it does not yet exist
+            Connection conn = DriverManager.getConnection("jdbc:hsqldb:TwitterClientDB", "CMPSC380","");
+                                            
+			// Initialize a menu and run cycle marker
+			int menu;
+			boolean quit = false;
 	
-		do {
-			// Gives the users a menu to choose a feature from.
-		   	System.out.println("Please type a number choice from the menu below:");
-		   	System.out.println("\t1. View Home Timeline");
-		    System.out.println("\t2. Update status from \"Tweets.txt\" file");
-		    System.out.println("\t3. Update status from \"Tweets.txt\" file and view Home Timeline");
-		 	System.out.println("\t4. Generate and tweet course notification for CS380s2013");
-		   	System.out.println("\t5. Quit");
-		   	
-		   	tc.sc = new Scanner(System.in);
-		   	menu = tc.sc.nextInt();
-		   	
-		   	// Perform the given menu function.
-		   	switch(menu) {
-		   	    case 1:
-		   	    	// View the home timeline
-		   	        tc.viewHome();
-		   	        break;
-		   	    case 2:
-		   	    	// Post a status
-		   	        tc.postStatus();
-		   	        break;
-		   	    case 3:
-		   	    	// Post a status the view home timeline
-		   	        tc.postStatus();
-		   	        tc.viewHome();
-		   	        break;
-		   	    case 4:
-		   	    	// Send a message to the CS380 class
-		   	        tc.courseMessage();
-		   	        break;
-		   	    case 5:
-		   	    	// Exit *sadface*
-		   	        quit = true;
-		   	        break;
-		   	    default:
-		   	        System.out.println("Invalid menu option, please choose from the list below...\n");
-		   	}
-		} while(!quit);
-		
-		System.out.println("\nNow exiting :'(");
+			do {
+				// Gives the users a menu to choose a feature from.
+			   	System.out.println("Please type a number choice from the menu below:");
+			   	System.out.println("\t1. View Home Timeline");
+				System.out.println("\t2. Update status from \"Tweets.txt\" file");
+				System.out.println("\t3. Update status from \"Tweets.txt\" file and view Home Timeline");
+			 	System.out.println("\t4. Generate and tweet course notification for CS380s2013");
+			   	System.out.println("\t5. Quit");
+			   	
+			   	tc.sc = new Scanner(System.in);
+			   	menu = tc.sc.nextInt();
+			   	
+			   	// Perform the given menu function.
+			   	switch(menu) {
+			   	    case 1:
+			   	    	// View the home timeline
+			   	        tc.viewHome(conn);
+			   	        break;
+			   	    case 2:
+			   	    	// Post a status
+			   	        tc.postStatus(conn);
+			   	        break;
+			   	    case 3:
+			   	    	// Post a status the view home timeline
+			   	        tc.postStatus(conn);
+			   	        tc.viewHome(conn);
+			   	        break;
+			   	    case 4:
+			   	    	// Send a message to the CS380 class
+			   	        tc.courseMessage();
+			   	        break;
+			   	    case 5:
+			   	    	// Exit *sadface*
+			   	        quit = true;
+			   	        break;
+			   	    default:
+			   	        System.out.println("Invalid menu option, please choose from the list below...\n");
+			   	}
+			} while(!quit);
+			
+			//Connection closes and program prints goodbye
+			conn.close();
+			System.out.println("\nNow exiting :'(");
+		} catch (Exception e) {
+			// Print out the error message
+            System.out.println(e);
+            e.printStackTrace();
+		}
     }
     
     /*
      * Uses twitter API to get contents of the home imeline
      * and print it to the screen
      *
+     * @param conn acccepts a sql connection
 	 * @return No return value
+	 * @exception throws sql exception if it fails
      */
-    public void viewHome(){
+    public void viewHome(Connection conn) throws SQLException {
 		// Display current Tweets in my stream of messages
 		try {
 
+	        // Create a statement object
+	        Statement stat = conn.createStatement();
+        
 			// Gets Twitter instance with default credentials
 			Twitter twitter = new TwitterFactory().getInstance();
 			User user = twitter.verifyCredentials();
 			List<Status> statuses = twitter.getHomeTimeline();
 			
+			// Try to drop the table
+        	try {
+        	    stat.executeUpdate("DROP TABLE Tweets");
+       		} catch (SQLException e) {}    // Ignore Exception, because the table may not yet exist
+       		
+       		// For compatibility to other database, use varchar(255)
+        	// In HSQL Database Engine, length is unlimited, like Java Strings
+        	stat.execute("CREATE TABLE Tweets (Date varchar(10),User varchar(15),Message varchar(140) NOT NULL,Offline int DEFAULT 0 NOT NULL)");
+
+			// Store the list of statuses in a database for offline retrieval
+			for(Status status : statuses) {
+				stat.executeUpdate("INSERT INTO Tweets (User,Message,Offline) VALUES (\"" + status.getUser().getScreenName() + "\",\"" + status.getText() + "\",1)");
+			}
+			System.out.println("debug");
+			// Now execute the search query
+       		ResultSet result = stat.executeQuery("SELECT User,Message FROM Tweets WHERE Offline = '1'");
+
 			// Print out the home stream
 			System.out.println("Showing @" + user.getScreenName() + "'s home timeline.\n");
-			for(Status status : statuses) {
-				System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
-			}
+			
+        	// Moves to the next record until no more records in home stream
+        	while (result.next()) {
+            	// Print the first column of the result
+            	// could use also getString("Path")
+            	System.out.println("@" + result.getString("User") + " - " + result.getString("Message"));
+        	}
+
+        	// Close the connection and resultset
+			stat.close();
+        	result.close();
 			
 			System.out.println("\n");
 			
 		} catch(TwitterException te) {
 			te.printStackTrace();
-			System.out.println("Failed to get timeline: " + te.getMessage() + "\n");
+			System.out.println("Failed to get timeline: " + te.getMessage() + "\n\n Displaying offline timeline:\n");
+			
+			// Create a statement object
+	        Statement stat = conn.createStatement();
+			
+			// Now execute the search query
+       		ResultSet result = stat.executeQuery("SELECT User,Message FROM Tweets WHERE Offline = '1'");
+			
+        	// Moves to the next record until no more records in home stream
+        	while (result.next()) {
+            	// Print the first column of the result
+            	// could use also getString("Path")
+            	System.out.println("@" + result.getString("User") + " - " + result.getString("Message"));
+        	}
+
+        	// Close the connection and resultset
+			stat.close();
+        	result.close();
+			
+			System.out.println("\n");
 		}
     }
 
     /*
      * Method posts statuses from file to the authenticated twitter account
      *
-     * @return No return value
+     * @param conn acccepts a sql connection
+	 * @return No return value
+	 * @exception throws sql exception if it fails
      */
-    public void postStatus(){
+    public void postStatus(Connection conn) throws SQLException {
         // Store tweets without database until sent.
 	    ArrayList<Tweet> validTweets = new ArrayList<Tweet>();
 	    ArrayList invalidTweetMessages = new ArrayList<String>();
